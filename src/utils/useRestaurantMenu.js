aa1742266c20d8731644ef8175f98d9f7414ef39
alankrita.mohapatra <alankrita.mohapatra@accenture.com>
2024-09-07 20:42:37 +0530

    PROXY_URL added

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -1,5 +1,5 @@
 import { useState,useEffect } from "react";
-import { MENU_URL } from "./constants";
+import { MENU_URL,PROXY_URL } from "./constants";
 
 const useRestaurantMenu  = (resId) =>{
     const [resInfo,setResInfo]= useState(null);
@@ -10,7 +10,7 @@ const useRestaurantMenu  = (resId) =>{
         
         
         const fetchMenu = async () =>{
-            const data = await fetch(MENU_URL + resId);
+            const data = await fetch( PROXY_URL + MENU_URL + resId);
             const json = await data.json();
             setResInfo(json.data);
     
