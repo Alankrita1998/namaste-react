9458caf7895753088d30c577c2bc7dd19f65013b
alankrita.mohapatra <alankrita.mohapatra@accenture.com>
2024-09-09 00:44:36 +0530

    trial

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,10 +9,9 @@ export const FOOD = "https://www.annmariegianni.com/wp-content/uploads/2014/05/b
 export const CONTACT = "https://img.freepik.com/premium-vector/young-man-using-laptop-computer-desk-working-online-illustration_24877-69116.jpg?w=2000";
 // export const RESTAURANT_LIST ="https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 export const EMPTY = "https://tse2.mm.bing.net/th?id=OIP.VEtjKjhwDm_M6AXOQmbZbwHaEX&pid=Api&P=0&h=220"; 
-export const BACKEND_URL = "https://cors-proxy-backend-mw56u2m81-alankrita-mohapatras-projects.vercel.app";
+export const BACKEND_URL = "https://cors-proxy-backend-d45xvt6fu-alankrita-mohapatras-projects.vercel.app";
 export const RESTAURANT_LIST = `${BACKEND_URL}/api/restaurants?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING`;
 export const MENU_URL = `${BACKEND_URL}/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=`;
 export const CDN_URL = `${BACKEND_URL}/api/cdn-image/`; 
-
 
 
