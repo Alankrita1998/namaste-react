5b9cd20fce3b3d001f071ac287865e5446270ea4
alankrita.mohapatra <alankrita.mohapatra@accenture.com>
2024-09-17 19:44:59 +0530

    css changes

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,23 +1,26 @@
 import { CDN_URL, STAR} from "../utils/constants";
 const RestaurantCard = (props) => {
     const {resData} = props;
-    const {cloudinaryImageId,name,cuisines,avgRating,costForTwo,sla} = resData?.info
+    const {cloudinaryImageId,name,cuisines,avgRating,costForTwo,sla,aggregatedDiscountInfoV3} = resData?.info
     return(
-        <div className = "leading-relaxed w-[16vw] h-[42vh] bg-[#edf5f4] m-4 rounded-lg p-1 shadow-lg border-2 shadow-gray-500 transition-transform duration-100 ease-in-out hover:cursor-pointer hover:scale-90 text-black overflow-hidden" >
-            <img  className="w-72 h-32 object-cover rounded-2xl p-1 pb-1" loading="lazy" src={CDN_URL + cloudinaryImageId}  alt = {name}/>
+        <div className = "leading-relaxed w-[18vw] h-[42vh] bg-[#edf5f4] m-4 rounded-xl p-1 shadow-lg border-2 shadow-gray-500 transition-transform duration-100 ease-in-out hover:cursor-pointer hover:scale-90 text-black overflow-hidden" >
+            <div class="relative border-transparent">
+            <img  className="w-72 h-32 object-cover rounded-xl pb-1" loading="lazy" src={CDN_URL + cloudinaryImageId}  alt = {name}/>
+            <div class="absolute bottom-0 rounded-xl left-0 right-0 h-1/3   bg-gradient-to-t from-black to-transparent "><span className="pl-2 text-start font-serif font-extrabold  text-[#edf5f4]  text-sm">{aggregatedDiscountInfoV3 ? aggregatedDiscountInfoV3.header + (" ") + aggregatedDiscountInfoV3.subHeader : ""}</span></div>
+            </div>
                 <div className="mx-2 my-1 overflow-hidden  ">
-                <h3 className="res-name font-bold text-[0.88rem] pb-1 ">{name}</h3>
+                <h3 className="res-name font-bold text-[0.92rem] pb-1 ">{name}</h3>
                 <div className="flex ">
                 <img className="w-4 h-4 " src={STAR}/>
                 <div
                 className="text-[0.8rem] font-semibold px-1">
                 {avgRating}
                 </div>
-                <h4 className="text-[0.8rem] font-semibold px-1 ">{ "• " + sla.slaString}</h4>
+                <h4 className="text-[0.8rem] font-semibold px-1 ">{ "• " +  ("  ") + sla.slaString}</h4>
                 </div>
                 <h4 className="res-cuisines  "> {cuisines.join(", ")}</h4> 
                 
-                <h4 className="text-sm font-serif font-normal text-gray-900 ">{costForTwo}</h4>
+                <h4 className="text-sm font-serif  font-bold text-gray-900 ">{costForTwo}</h4>
                 </div>
         </div>
  
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -2,22 +2,22 @@ const Shimmer = () => {
 
     return(
         <div className="flex flex-wrap mx-12  shadow-lg justify-evenly">
-            <div className = "w-[16vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
-            <div className = "w-[16vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
-            <div className = "w-[16vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
-            <div className = "w-[16vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
-            <div className = "w-[16vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
-            <div className = "w-[16vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
-            <div className = "w-[16vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
-            <div className = "w-[16vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
-            <div className = "w-[16vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
-            <div className = "w-[16vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
-            <div className = "w-[16vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
-            <div className = "w-[16vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
-            <div className = "w-[16vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
-            <div className = "w-[16vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
-            <div className = "w-[16vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
-            <div className = "w-[16vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
+            <div className = "w-[18vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
+            <div className = "w-[18vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
+            <div className = "w-[18vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
+            <div className = "w-[18vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
+            <div className = "w-[18vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
+            <div className = "w-[18vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
+            <div className = "w-[18vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
+            <div className = "w-[18vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
+            <div className = "w-[18vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
+            <div className = "w-[18vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
+            <div className = "w-[18vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
+            <div className = "w-[18vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
+            <div className = "w-[18vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
+            <div className = "w-[18vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
+            <div className = "w-[18vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
+            <div className = "w-[18vw] h-[42vh]  bg-gray-100 m-4 rounded-lg p-1 " ></div>
 
         </div>
     )
